15f00c1a8a6c82e3333211ef1f541bf863473078
kartikgupta666 <kartikgangil@gmail.com>
2025-05-14 01:33:57 +0530

    fixing Backend

--- Backend/config/Passport.js
+++ Backend/config/Passport.js
@@ -9,7 +9,7 @@ dotenv.config()
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback',
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
 },
     async (accessToken, refreshToken, profile, done) => {
         const user = await User.findOne({ Id: profile.id })
@@ -35,7 +35,7 @@ passport.use(new GoogleStrategy({
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: '/auth/github/callback'
+    callbackURL: process.env.GITHUB_CALLBACK_URL
 },
     async (accessToken, refreshToken, profile, done) => {
         
