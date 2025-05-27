ddd2139393201bbeae97aeedfb7f2172b8aff391
kartikgupta666 <kartikgangil@gmail.com>
2025-05-28 01:24:14 +0530

    fixing frontend

--- Frontend/src/Components/Main.jsx
+++ Frontend/src/Components/Main.jsx
@@ -2,7 +2,7 @@ import { IoCloudDownloadOutline } from "react-icons/io5"
 import axios from "axios"
 import Loader from './Loader'
 import { ToastContainer, toast } from 'react-toastify';
-import MD_viewer from "./MD_viewer"
+
 import { useEffect, useState } from 'react'
 // import { useNavigate } from 'react-router-dom'
 function Main() {
