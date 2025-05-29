1fe744ba4bd7d9c7f0b7bc247bc666f9df785bcc
kartikgupta666 <kartikgangil@gmail.com>
2025-05-29 11:54:04 +0530

    fix ads.txt file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Routes , Route } from 'react-router-dom'
 import Main from './Components/Main'
 import Auth from './Components/Auth'
+
 const App = () => {
   return (
     <Routes>
       <Route exact path='/' element={<Main></Main> } />
       <Route exact path='/auth' element={<Auth></Auth> } />
+      {/* <Route exact path='/ads.txt' element={<Ads></Ads>} /> */}
     </Routes>
   )
 }
