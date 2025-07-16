a887068dac924c9449018645b29c41bb74b1c0d0
kartikgupta666 <kartikgangil@gmail.com>
2025-07-16 21:24:34 +0530

    adding analytics

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,16 +3,18 @@ import { Routes, Route } from 'react-router-dom'
 import Main from './Components/Main'
 import LandingPage from './Components/Landing_page'
 // import Auth from './Components/Auth'
-
+import { Analytics } from "@vercel/analytics/next"
 const App = () => {
   return (
-    <Routes>
-      <Route exact path='/' element={<LandingPage></LandingPage> } />
-      <Route exact path='/main' element={<Main></Main> } />
-      {/* <Route exact path='/auth' element={<Auth></Auth> } /> */}
-      {/* <Route exact path='/ads.txt' element={<Ads></Ads>} /> */}
-    </Routes>
-
+    <>
+      <Routes>
+        <Route exact path='/' element={<LandingPage></LandingPage>} />
+        <Route exact path='/main' element={<Main></Main>} />
+        {/* <Route exact path='/auth' element={<Auth></Auth> } /> */}
+        {/* <Route exact path='/ads.txt' element={<Ads></Ads>} /> */}
+      </Routes>
+      <Analytics />
+    </>
   )
 }
 
