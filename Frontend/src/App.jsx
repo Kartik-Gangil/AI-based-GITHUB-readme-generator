e230e78dc3b3d3eb7f30e5130dd18c60020d8965
kartikgupta666 <kartikgangil@gmail.com>
2025-07-16 22:39:43 +0530

    fixing route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Main from './Components/Main'
 import LandingPage from './Components/Landing_page'
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route exact path='/' element={<LandingPage></LandingPage>} />
-        <Route exact path='/main' element={<Main></Main>} />
-        {/* <Route exact path='/auth' element={<Auth></Auth> } /> */}
-        {/* <Route exact path='/ads.txt' element={<Ads></Ads>} /> */}
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<LandingPage/>} />
+          <Route exact path='/main' element={<Main/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
